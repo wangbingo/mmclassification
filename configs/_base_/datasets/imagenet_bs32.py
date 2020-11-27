79bad4c055dbf7e309b61afb492839c2a5b3d124
王兵 <wangbing@92-68-1-106.biz.kpn.net>
2020-11-27 12:24:20 +0800

    samples_per_gpu=16

--- configs/_base_/datasets/imagenet_bs32.py
+++ configs/_base_/datasets/imagenet_bs32.py
@@ -21,7 +21,7 @@ test_pipeline = [
     dict(type='Collect', keys=['img'])
 ]
 data = dict(
-    samples_per_gpu=32,
+    samples_per_gpu=16,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
