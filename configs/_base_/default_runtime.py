37a6d4c800a06e9bdac8bc43f31386d7348173b3
王兵 <wangbing@106.1.68.92.in-addr.arpa>
2020-11-27 13:43:29 +0800

    load_from = 'resnest50_converted-1ebf0afe.pth'

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -10,6 +10,7 @@ log_config = dict(
 # yapf:enable
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = '../../input/resnest50-pretrained-weight/resnest50_converted-1ebf0afe.pth'     #####
+#load_from = '../../input/resnest50-pretrained-weight/resnest50_converted-1ebf0afe.pth'     #####
+load_from = 'resnest50_converted-1ebf0afe.pth'     #####
 resume_from = None
 workflow = [('train', 1)]
