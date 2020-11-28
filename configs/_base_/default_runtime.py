482a2b24248a5a0efa2e0063f972cadd8c70021b
王兵 <wangbing@92-68-1-106.biz.kpn.net>
2020-11-28 14:16:40 +0800

    load_from = None

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -11,6 +11,7 @@ log_config = dict(
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 #load_from = '../../input/resnest50-pretrained-weight/resnest50_converted-1ebf0afe.pth'     #####
-load_from = 'resnest50_converted-1ebf0afe.pth'     #####
+#load_from = 'resnest50_converted-1ebf0afe.pth'     #####
+load_from = None
 resume_from = None
 workflow = [('train', 1)]
